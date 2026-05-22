e7e2a23ce98005a776de78a33138643bf3e3e109
Arseniy Uspenskiy <uspenskiyarseniy@gmail.com>
2026-05-22 15:18:28 +0200

    Initial commit

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    
+    public static GameInput Instance { get; private set; }
+
+    private PlayerInputActions playerInputActions;
+
+    private
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D playerBody;
+    [SerializeField] private CapsuleCollider2D playerCapsuleCollider;
+
+    private float moveSpeed = 7f;
+
+    private void Update() {
+        
+    }
+
+    private void PlayerMove() {
+        
+    }
+
+}
